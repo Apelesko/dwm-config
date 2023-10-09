a0ecfa6dd021c37c57456644bc4ff3cb814560ce
andrew <apelesko@gmail.com>
2023-10-09 19:17:07 -0400

    fixed time format in status script, fixed monitor timeout in autostart script

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -4,3 +4,4 @@ $HOME/github/dwm-config/scripts/status &
 /usr/bin/lxpolkit &
 feh --bg-fill --randomize --recursive $HOME/Pictures/backgrounds &
 picom --config "$HOME/.config/picom.conf"
+xset s off -dpms #disabling things like turing off the monitor
